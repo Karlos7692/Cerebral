d5e3211232bce1561fc9a46db4ab8ea83b339f8c
Karl Nelson <k.c.nelson7692@gmail.com>
2014-01-28 15:44:33 +1100

    CV Analyser

---
+++ Matlab/cvanalyser.m
@@ -0,0 +1,40 @@
+function [CVPred] = cvanalyser(NN, RawIn, CVIn, CVOut)
+%CVANALYSER Summary of this function goes here
+%   Detailed explanation goes here
+    hold off
+    cvsize = size(CV, 1);
+    
+    Total = [RawIn ; CVIn];
+    
+    if(strcmp(NN.type, 'tem'))
+        Total = prepareTNN(NN, Total);
+    end
+    
+    CVPred = zeros(size(CVOut));
+    TotalPred = predict(NN, Total);
+    
+    CVBPreds = TotalPred(end-cvsize+1:end, :);
+    
+    
+    %TODO change binary convertions to allow matrix calculations
+    %TODO change to allow thresholding
+    %TODO create convertion function
+    for i = 1:size(CVOut,1)
+        CVPred(i) = bdvecToReal(CVBPreds(i,:));
+    end
+    
+    %Plot Predicted values overlayed Actual Values
+    plot(1:length(CVOut), CVOut, 'r');
+    hold on
+    plot(1:length(CVOut), CVPred, 'b');
+    plot(1:length(CVOut), CVOut - CVPred, 'g');
+    xlabel('Time');
+    ylabel('Stock Price');
+    title('Cross Validation Comparision');
+    
+    
+    hold off
+    
+
+end
+
