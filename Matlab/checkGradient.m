7a50bd4f8a615637645473a5eb85a8dbd18ebf6f
Karl Nelson <k.c.nelson7692@gmail.com>
2014-02-08 18:45:36 +1100

    Gradient Checking

---
+++ Matlab/checkGradient.m
@@ -0,0 +1,26 @@
+function diff = checkGradient(NN, Y, X, lambda)
+
+    X = maintainNN(NN,X);
+    [J, grad] = costFunc(NN, Y, X, lambda);
+    approx = approxGradient(NN, X, Y, lambda);
+    
+    
+    %Manual Gradient checking.
+    %disp([approx grad]);
+    %fprintf(['The above two columns you get should be very similar.\n' ...
+    %     '(Left-Your Numerical Gradient, Right-Analytical Gradient)\n\n']);
+    
+    
+   
+    % Evaluate the norm of the difference between two solutions.  
+    % If you have a correct implementation, and assuming you used EPSILON = 0.0001 
+    % in computeNumericalGradient.m, then diff below should be less than 1e-9
+    diff = norm(approx-grad)/norm(approx+grad);
+    
+    fprintf(['If your backpropagation implementation is correct, then \n' ...
+         'the relative difference will be small (less than 1e-9). \n' ...
+         '\nRelative Difference: %g\n'], diff);
+end
+
+
+
